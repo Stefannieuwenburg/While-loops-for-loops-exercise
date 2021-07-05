039c06ab0858e2d781ef2b42c575fc3f547f275d
Stefan Nieuwenburg <82890714+Stefannieuwenburg@users.noreply.github.com>
2021-07-05 20:02:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+/*var age = 5;
+while (age <10){
+    console.log("you age is les than 10");
+    age++;
+}
+document.write("you are now over 10 years old");
+
+for (age = 5; age < 10; age++){
+    console.log("your age is les than 10 years old");
+}
+console.log("you are now over 10 years old");
+
+// while
+let w = 0;
+while (w < 5) {
+  console.log(`This is while iteration number ${w}`);
+  w++;
+}
+
+ for
+for (let i = 0; i < 5; i++) {
+  console.log(`This is for iteration number ${i}`);
+}
+*/
+
+
